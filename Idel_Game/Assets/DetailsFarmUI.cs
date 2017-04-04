e8bf8b2a87c0b646cfc9cedab8eb51e248efb78c
AlmoXer <leonhard_marx@hotmail.de>
2017-04-05 01:38:57 +0200

    DetailsFarmUI (Funktionen Fehlen noch)

---
+++ Idel_Game/Assets/DetailsFarmUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetailsFarmUI : MonoBehaviour {
+
+    public Text title;
+    public Image iconFarm;
+    public Text levelFram;
+    public Text incomeOffline;
+    public Text incomeOnline;
+    public Text countFelder;
+    public Text costUpgrade;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Idel_Game/Assets/ScreenFader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFader : MonoBehaviour {
+
+    public GameObject farmDetails;
+    [SerializeField]
+    private GameObject lastScreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenFarmDetails()
+    {
+        farmDetails.SetActive(true);
+        lastScreen = farmDetails;
+    }
+
+    public void CloseLastScreen()
+    {
+        lastScreen.SetActive(false);
+        lastScreen = null;
+    }
+}
