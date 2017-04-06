a93fec76c82b4795e3f8222e26bacbdad28e5e26
AlmoXer <leonhard_marx@hotmail.de>
2017-04-06 16:55:44 +0200

    PlayerStats

--- Idel_Game/Assets/PlayerStats.cs
+++ Idel_Game/Assets/PlayerStats.cs
@@ -18,9 +18,65 @@ public class PlayerStats : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
-        incomeOffline.text = PlayerProfile.player.incomeOffline.ToString() + "/s";
-        incomeOnline.text = PlayerProfile.player.incomeOnline.ToString() + "/s";
-        money.text = PlayerProfile.player.money.ToString();
+        //Income Offline Anzeigen
+        for (int i = PlayerProfile.player.incomeOffline.Length - 1; i >= 0; i--)
+        {
+            if (i == 0)
+                Debug.Log("");
+
+            if (PlayerProfile.player.incomeOffline[i] > 0)
+            {
+                string text;
+                if (i > 0)
+                    text = PlayerProfile.player.incomeOffline[i].ToString() + "." + (PlayerProfile.player.incomeOffline[i - 1] / 10).ToString() + " " + (char)(i + 97);
+                else
+                    text = PlayerProfile.player.incomeOffline[i].ToString() + " " + (char)(i + 97);
+
+
+                incomeOffline.text = text;
+                break;
+            }
+        }
+
+        //Income Online Anzeigen
+        for (int i = PlayerProfile.player.incomeOnline.Length - 1; i >= 0; i--)
+        {
+            if (PlayerProfile.player.incomeOnline[i] > 0)
+            {
+                string text;
+                if (i > 0)
+                    text = PlayerProfile.player.incomeOnline[i].ToString() + "." + (PlayerProfile.player.incomeOnline[i - 1] / 10).ToString() + " " + (char)(i + 97) + "/s";
+                else
+                    text = PlayerProfile.player.incomeOnline[i].ToString() + " " + (char)(i + 97) + "/s";
+
+
+                incomeOnline.text = text;
+                break;
+            }
+        }
+
+
+        //Geld Anzeigen
+        for (int i = PlayerProfile.player.money.Length - 1; i >= 0; i--)
+        {
+            if (PlayerProfile.player.money[i] > 0)
+            {
+                string text;
+                if (i > 0)
+                    text = PlayerProfile.player.money[i].ToString() + "." + (PlayerProfile.player.money[i - 1] / 10).ToString() + " " + (char)(i + 97);
+                else
+                    text = PlayerProfile.player.money[i].ToString() + " " + (char)(i + 97);
+
+
+                money.text = text;
+                break;
+            }
+
+            if (i == 0)
+                money.text = "Pleite";
+        }
+
+        //Titel Anzeigen
         rank.text = PlayerProfile.player.rank;
     }
 
